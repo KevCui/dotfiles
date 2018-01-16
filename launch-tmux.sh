eaac5cd3c0420895a49933314c3f4e92bfbf9c10
Kevin Cui <krazycavin@gmail.com>
2018-01-16 20:33:30 +0100

    Include tmux launcher file

---
+++ launch-tmux.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+# Main |-
+tmux new-session -d -s main -n main
+tmux split-window -h -p 40 'while true; do tput clear; date;echo;task ls; sleep 300; done'
+tmux split-window -v 'task shell'
+
+# File Manager
+tmux new-window -n "file" 'ranger ~/.config/ranger'
+tmux select-pane -t 0
+
+# Script |
+tmux new-window -n "other"
+tmux split-window -h -p 50
+tmux select-pane -t 0
+
+tmux select-window -t main
+tmux attach
