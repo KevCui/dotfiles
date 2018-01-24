99e684f2464134ab13c4fd09b254f6182038caf9
Kevin Cui <krazycavin@gmail.com>
2018-01-24 16:05:05 +0100

    Change panes position

--- launch-tmux.sh
+++ launch-tmux.sh
@@ -5,13 +5,13 @@ tmux new-session -d -s main -n main
 tmux split-window -h -p 40 'while true; do tput clear; date;echo;task ls; sleep 300; done'
 tmux split-window -v 'task shell'
 
-# File Manager
-tmux new-window -n "file" 'source .zshrc; ranger'
-tmux select-pane -t 0
-
 # Script |
 tmux new-window -n "other"
 tmux split-window -h -p 50
+tmux select-pane -t 0
+
+# File Manager
+tmux new-window -n "file" 'source .zshrc; ranger'
 tmux select-pane -t 0
 
 tmux select-window -t main
