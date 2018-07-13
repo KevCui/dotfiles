dd2c85696c0e6d6966eb19b76d5c28ac38689c0b
Kevin Cui <krazycavin@gmail.com>
2018-07-14 01:24:26 +0200

    st patch to support scroll back

--- st.config.h
+++ st.config.h
@@ -156,8 +156,15 @@ static unsigned int defaultattr = 11;
  */
 static MouseShortcut mshortcuts[] = {
 	/* button               mask            string */
-	{ Button4,              XK_ANY_MOD,     "\031" },
-	{ Button5,              XK_ANY_MOD,     "\005" },
+    { Button4,              XK_NO_MOD,      "\031" },
+    { Button5,              XK_NO_MOD,      "\005" },
+
+};
+
+MouseKey mkeys[] = {
+    /* button               mask            function        argument */
+    { Button4,              ShiftMask,      kscrollup,      {.i =  1} },
+    { Button5,              ShiftMask,      kscrolldown,    {.i =  1} },
 };
 
 /* Internal keyboard shortcuts. */
@@ -183,6 +190,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ TERMMOD,              XK_I,           iso14755,       {.i =  0} },
     { ControlMask,          XK_o,           externalpipe,   { .v = openurlcmd } },
+    { ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
+    { ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
 
 /*
