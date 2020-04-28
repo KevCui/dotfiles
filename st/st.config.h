7931d2f0e5447dc040feb527fbe282a40e0dc6ee
Kevin Cui <krazycavin@gmail.com>
2020-04-28 10:10:11 +0200

    st: increase borderpx to 5

--- st/st.config.h
+++ st/st.config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "%font%:size=%size%:antialias=true:autohint=true";
-static int borderpx = 2;
+static int borderpx = 5;
 
 /*
  * What program is execed by st depends of these precedence rules:
