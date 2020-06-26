536b74542b805b04fdfb416e97acd69219658bf6
Kevin Cui <krazycavin@gmail.com>
2020-06-26 14:36:27 +0200

    st: change letter spacing and line spacing

--- st/st.config.h
+++ st/st.config.h
@@ -26,8 +26,8 @@ char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
-static float cwscale = 1.0;
-static float chscale = 1.0;
+static float cwscale = 0.9;
+static float chscale = 0.95;
 
 /*
  * word delimiter string
